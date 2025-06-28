61d52d615d39b17ba6debe28282def46b2c92a90
Gabriel Gomez <132381579+Gopi1794@users.noreply.github.com>
2025-06-28 19:23:10 -0300

    Corrijo configuración en ecosystem.config.js

---
+++ server/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.export = {
+    apps: [
+        {
+            name: "gestor-de-inventario",
+            script: "npm",
+            args: "run dev",
+            env: {
+                NODE_ENV: "development",
+                ENV_VAR1: "enviromentgit -variable",
+            }
+        }
+    ]
+}
